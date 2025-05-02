df596a91af2a9bddcfe5126dacfba92533e1bd88
Roy Okello <roy@stelar.xyz>
2025-05-02 11:26:26 +0300

    fix: imports

--- src/astreum/node/validation/block.py
+++ src/astreum/node/validation/block.py
@@ -1,29 +1,20 @@
 class Block:
-    @classmethod
-    def from_bytes(cls, validator) -> 'Block':
-        """
-        Deserialize an Account from its byte representation.
-        
-        Expected format: [balance, code, counter, data]
-        
-        The public_key (and optional secret_key) must be provided separately.
-    """
-        decoded = bytes_format.decode(data)
-        balance, code, counter, account_data = decoded
-        return cls(public_key, balance, code, counter, account_data, secret_key=secret_key)
+
+    def __init__(self):
+        pass
     
+    @classmethod
+    def from_bytes(cls) -> 'Block':
+        """
+        Deserialize a block from its byte representation.
+    """
+        return cls()
+
     def to_bytes(self) -> bytes:
         """
-        Serialize the Account into bytes.
-        
-        Format: [balance, code, counter, data]
+        Serialize the block into bytes.
         """
-        return bytes_format.encode([
-            self.balance,
-            self.code,
-            self.counter,
-            self.data
-        ])
+        return b""
 
 class Chain:
     def __init__(self, latest_block: Block):
