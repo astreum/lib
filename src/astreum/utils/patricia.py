3008cb708afe20df23b42500fa2ef5d738e6f424
Roy Okello <roy@stelar.xyz>
2025-07-09 16:05:36 +0300

    feat: added patricia trie

--- src/astreum/utils/patricia.py
+++ src/astreum/utils/patricia.py
@@ -1,14 +1,17 @@
 import blake3
-from typing import List
+from typing import Callable, Dict, List, Optional
 from astreum import format
 
+EMPTY_HASH = b"\x00" * 32
 
 class PatriciaNode:
-    def __init__(self,
-                 key_len: int,
-                 key_bits: bytes,
-                 value: bytes,
-                 children: List[bytes]):
+    def __init__(
+        self,
+        key_len: int,
+        key_bits: bytes,
+        value: bytes,
+        children: List[bytes]|None = None
+    ):
         self.key_len   = key_len
         self.key_bits  = key_bits
         self.value     = value
@@ -30,3 +33,13 @@ class PatriciaNode:
         if self._hash is None:
             self._hash = blake3.blake3(self.to_bytes()).digest()
         return self._hash
+
+class PatriciaTrie:
+    def __init__(
+        self,
+        node_get: Callable[[bytes], Optional[bytes]],
+        root_hash: Optional[bytes] = None,
+    ) -> None:
+        self._node_get = node_get
+        self.nodes: Dict[bytes, bytes] = {}
+        self.root_hash: Optional[bytes] = root_hash
